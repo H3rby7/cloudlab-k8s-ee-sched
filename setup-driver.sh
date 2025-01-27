09cfdf8d84977e2ed74f93d5a1fbf54986137153
h3rby7 <herbielutz@gmx.net>
2025-01-27 19:52:48 +0100

    Added script to wait for all nodes before node-0 specific scripts

--- setup-driver.sh
+++ setup-driver.sh
@@ -13,6 +13,25 @@ export SRC=`dirname $0`
 cd $SRC
 . $SRC/setup-lib.sh
 
+wait_for_all_nodes() {
+	echo "Waiting for all nodes to become reachable [$NODEIPS]"
+	ip_list=($NODEIPS)
+	while :; do
+		all_reachable=true
+		for ip in "${ip_list[@]}"; do
+			if ! ping -c 1 -W 1 "$ip" &> /dev/null; then
+				echo "Waiting for $ip to become reachable..."
+				all_reachable=false
+			fi
+		done
+		if $all_reachable; then
+			echo "All IPs are reachable!"
+			break
+		fi
+		sleep 5
+  done
+}
+
 # Don't run setup-driver.sh twice
 if [ -f $OURDIR/setup-driver-done ]; then
     echo "setup-driver already ran; not running again"
@@ -27,6 +46,8 @@ for script in $ALLNODESCRIPTS ; do
     fi
 done
 if [ "$HOSTNAME" = "node-0" ]; then
+		# node-0 needs other nodes to be ready, so we wait.
+		wait_for_all_nodes
     for script in $HEADNODESCRIPTS ; do
 	cd $SRC
 	$SRC/$script | tee - /local/logs/${script}.log 2>&1
