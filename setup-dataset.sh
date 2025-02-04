36846d3e5583a09e54d50c0db4d9d5cc3eba9880
h3rby7 <herbielutz@gmx.net>
2025-02-04 01:44:54 +0100

    added min_max_normalized_service_metrics.tsv

--- setup-dataset.sh
+++ setup-dataset.sh
@@ -33,6 +33,10 @@ echo "Getting deployment_ts..."
 $SUDO wget -O deployment_ts.csv "${base_url}/deployment_ts.tsv"
 $SUDO mv deployment_ts.csv /dataset/
 
+echo "Getting min_max_normalized_service_metrics..."
+$SUDO wget -O min_max_normalized_service_metrics.csv "${base_url}/min_max_normalized_service_metrics.tsv"
+$SUDO mv min_max_normalized_service_metrics.csv /dataset/
+
 echo "Getting service_graphs..."
 $SUDO wget -O service_graphs.json "${base_url}/service_graphs.json"
 $SUDO mv service_graphs.json /dataset/
